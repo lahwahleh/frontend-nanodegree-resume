9b558534a28855984f9f87724b1d90f575a56eca
Olawale Ayodele <olawaleayodele@gmail.com>
2016-09-08 13:47:13 +0100

    updated resumeBuilder.js file with vars

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,15 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+$("#main").append("Olawale AYODELE");
+
+var email = " \n olawaleayodele@gmail.com"
+var newEmail = email.replace("olawaleayodele@gmail.com"," oayodele@chams.com")
+var awesomeThoughts = " \n I am Olawale and I am awesome";
+var funThoughts = awesomeThoughts.replace("awesome", "fun");
+
+console.log(awesomeThoughts);
+console.log(funThoughts);
+console.log(email);
+console.log(newEmail);
+
+$("#main").append(email);
+$("#main").append(newEmail);
+$("#main").append(awesomeThoughts);
