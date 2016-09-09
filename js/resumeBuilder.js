66c2b7eba5de6530de147d587ac61f402fd6c8c9
Olawale Ayodele <olawaleayodele@gmail.com>
2016-09-10 00:34:03 +0100

    Completed Bio, Work, Education and Projects using JSON in resumeBuilder.js file

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -14,7 +14,7 @@ console.log(newEmail);
 $("#main").append(email);
 $("#main").append(newEmail);
 $("#main").append(awesomeThoughts);
-*/
+
 
 var name = "Olawale AYODELE";
 var role = "Web Developer";
@@ -25,7 +25,7 @@ var formattedRole = HTMLheaderRole.replace("%data%", role);
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
 
-
+*/
 
 
 var bio = {
@@ -39,25 +39,78 @@ var bio = {
 		"location": "Matogun, Ogun State, Nigeria"
 		},
 	"biopic" : "images/olawale.jpg",
+	"welcomeMessage" : "The end determines the means!",
 	"skills" : [" HTML", " CSS", " Javascript", " Wordpress", " Java", " Python", " Git", " Android App Development", " Microsoft Office Suites"]
 
 }
 
 
-var work = {};
-work.position = "Web Developer";
-work.employer = "Chams Plc";
-work.city = "Victoria Island, Lagos";
 
-var education  = {};
-education["school"] = "OAU, Ile-Ife";
-education["city"] = "Osun State, Nigeria";
-education["years"] = "2006 - 2011";
+var education  = {
+	"schools": [{
+			"name": "Sanngo Sec Sch",
+			"city": "Agege, Lagos",
+			"major": "Sciences",
+			"yearOfGraduation": 2005
+		},
+
+		{
+			"name": "OAU, Ile-Ife",
+			"city": "Osun State, Nigeria",
+			"major": "Computer Engineering",
+			"yearOfGraduation": 2012
+		}
+	],
+
+	"onlineCourses": [{
+			"name": "Udacity",
+			"city": "Silicon Valley",
+			"major": "Frontend Web Development",
+			"yearOfGraduation": 2016
+		},
+
+		{
+			"name": "Indian Inst of Tech",
+			"city": "bangalore",
+			"major": "Innovation and IT Mgt",
+			"yearOfGraduation": 2016
+		}
+
+	]
+
+}
+
+var work = {
+	"jobs" : [
+				{"employer" : "Chams Plc",
+				  "title": "Software Development Intern",
+				  "dates" : "2016 - present" },
+
+				{"employer" : "IMR Nig Ltd",
+				  "title": "Industrial Trainee",
+				  "dates" : "2010 - 2011" }
+
+	]
+
+
+
+}
+
+var projects = {
+	"projects" : [
+				{"name" : "KCOBA Mobile App",
+				  "dates": "Aug - Sept, 2016",
+				  "description" : "If you havent got a clue, what to do, just right click here and select the Help" }
+
+		]
+
+
+
+}
 
 
 
 
-$("#main").append(work.position);
-$("#main").append(education["school"]);
+
 
 
