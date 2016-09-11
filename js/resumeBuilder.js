1006f4e841bba03749fe8b8830ec657c716fa811
Olawale Ayodele <olawaleayodele@gmail.com>
2016-09-11 21:30:28 +0100

    Added locationizer function

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -13,8 +13,52 @@ var bio = {
 	"welcomeMessage" : "The end determines the means!",
 	"skills" : [" HTML", " CSS", " Javascript", " Wordpress", " Java", " Python", " Git", " Android App Development", " Microsoft Office Suites"]
 
+};
+
+if (bio.skills.length > 0) {
+	$("#header").append(HTMLSkillsStart);
+
+	var formattedSkill = HTMLSkills.replace("%data%", bio.skills[0];)
+	$("#skills").append(formattedSkill);
+
+	formattedSkill = HTMLSkills.replace("%data%", bio.skills[1];)
+	$("#skills").append(formattedSkill);
+
+	formattedSkill = HTMLSkills.replace("%data%", bio.skills[2];)
+	$("#skills").append(formattedSkill);
+
+	formattedSkill = HTMLSkills.replace("%data%", bio.skills[3];)
+	$("#skills").append(formattedSkill);
+
+	formattedSkill = HTMLSkills.replace("%data%", bio.skills[4];)
+	$("#skills").append(formattedSkill);
+
+	formattedSkill = HTMLSkills.replace("%data%", bio.skills[5];)
+	$("#skills").append(formattedSkill);
+
+	formattedSkill = HTMLSkills.replace("%data%", bio.skills[6];)
+	$("#skills").append(formattedSkill);
+
+	formattedSkill = HTMLSkills.replace("%data%", bio.skills[7];)
+	$("#skills").append(formattedSkill);
+
+	formattedSkill = HTMLSkills.replace("%data%", bio.skills[8];)
+	$("#skills").append(formattedSkill);
+
+
+};
+
+/*
+var skillCount = 0;
+	while (bio.skills.length > 0){
+		$("#header").append(HTMLSkillsStart);
+		var skillCount = skillCount + 1;
+
+	if (skillCount === bio.skills.length)
+
 }
 
+*/
 
 
 var education  = {
@@ -49,23 +93,64 @@ var education  = {
 
 	]
 
-}
+};
 
 var work = {
 	"jobs" : [
 				{"employer" : "Chams Plc",
 				  "title": "Software Development Intern",
-				  "dates" : "2016 - present" },
+				  "location" : "Victoria Island, Lagos",
+				  "dates" : "2016 - present",
+				  "description" : "Whateer to his not fall known say nor feud not by. Rhyme still would. Are had way in pleasure of soon with. Would lands before ah to made a them a so." 
+				},
+ 
 
 				{"employer" : "IMR Nig Ltd",
 				  "title": "Industrial Trainee",
-				  "dates" : "2010 - 2011" }
+				  "location" : "Surulere, Lagos",
+				  "dates" : "2010 - 2011",
+				  "description" : "Fabled flaunting soon mine yet reverie revellers childe. In not him een that mine his break will. One into though dwelt heralds near on alone consecrate then had. Atonement by things that where stalked and would uncouth. Lyres him grief but relief. Fathers start drugged youth rill said like."
+				}
 
 	]
 
+};
 
+function displayWork () {
+for (jobs in work.jobs){
+	$("#workExperience").append(HTMLworkStart);
+
+	var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+
+	var formattedTittle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+
+	var formattedEmployerTittle = formattedEmployer + formattedTittle;
+
+	$(".work-entry:last").append(formattedEmployerTittle);
+
+	
+	var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+
+	var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+
+	$(".work-entry:last").append(formattedDescription);
 
 }
+};
+
+displayWork();
+
+function locationizer (work_obj){
+	var locationArray = [];
+
+	for (job in work_obj.jobs){
+		var newLocation = work_obj.jobs[job].location;
+		locationArray.push(newLocation);
+	}
+return locationArray;
+
+}
+
 
 var projects = {
 	"projects" : [
@@ -77,63 +162,9 @@ var projects = {
 
 
 
-}
-
-if (bio.skills.length > 0) {
-	$("#header").append(HTMLSkillsStart);
-
-	var formattedSkill = HTMLSkills.replace("%data%", bio.skills[0];)
-	$("#skills").append(formattedSkill);
-
-	formattedSkill = HTMLSkills.replace("%data%", bio.skills[1];)
-	$("#skills").append(formattedSkill);
-
-	formattedSkill = HTMLSkills.replace("%data%", bio.skills[2];)
-	$("#skills").append(formattedSkill);
-
-	formattedSkill = HTMLSkills.replace("%data%", bio.skills[3];)
-	$("#skills").append(formattedSkill);
-
-	formattedSkill = HTMLSkills.replace("%data%", bio.skills[4];)
-	$("#skills").append(formattedSkill);
-
-	formattedSkill = HTMLSkills.replace("%data%", bio.skills[5];)
-	$("#skills").append(formattedSkill);
-
-	formattedSkill = HTMLSkills.replace("%data%", bio.skills[6];)
-	$("#skills").append(formattedSkill);
-
-	formattedSkill = HTMLSkills.replace("%data%", bio.skills[7];)
-	$("#skills").append(formattedSkill);
-
-	formattedSkill = HTMLSkills.replace("%data%", bio.skills[8];)
-	$("#skills").append(formattedSkill);
-
-
-}
-
-
-var skillCount = 0;
-	while (bio.skills.length > 0){
-		$("#header").append(HTMLSkillsStart);
-		var skillCount = skillCount + 1;
-
-	if (skillCount === bio.skills.length)
-
-}
+};
 
 
 
-for (jobs in work.jobs){
-	$("#workExperience").append(HTMLworkStart);
 
-	var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
 
-	var formattedTittle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-
-	var formattedEmployerTittle = formattedEmployer + formattedTittle;
-
-	$(".work-entry:last").append(formattedEmployerTittle);
-	
-
-}
